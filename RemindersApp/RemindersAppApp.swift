8ba99e657f5a5d9396af7bcebda37f3dcf5c84b4
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2023-11-07 14:50:28 -0500

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by 宮川義之助 on 2023/11/07.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
